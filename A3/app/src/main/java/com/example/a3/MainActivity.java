72398363760594190f129a640a22f557f33afece
Furqaan <fali30@uic.edu>
2020-10-31 07:38:20 -0500

    Added fragments

--- A3/app/src/main/java/com/example/a3/MainActivity.java
+++ A3/app/src/main/java/com/example/a3/MainActivity.java
@@ -2,19 +2,44 @@ package com.example.a3;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.a3.TitlesFragment.ListSelectionListener;
+
+public class MainActivity extends AppCompatActivity implements ListSelectionListener {
 
     private static final String KABOOM_PERMISSION =
             "com.example.a3.edu.uic.cs478.f20.kaboom" ;
     private static final String A3_INTENT =
             "edu.uic.cs478.Receiver";
+
+
+    public static String[] mTitleArray;
+    public static String[] mQuoteArray;
+
+
+    private final QuotesFragment mQuoteFragment = new QuotesFragment();
+
+    // UB 2/24/2019 -- Android Pie twist: Original FragmentManager class is now deprecated.
+    //MH 7/7/2020 added support library in gradle module build file
+
+    FragmentManager mFragmentManager;
+
+    private FrameLayout mTitleFrameLayout, mQuotesFrameLayout;
+    private static final int MATCH_PARENT = LinearLayout.LayoutParams.MATCH_PARENT;
+    private static final String TAG = "QuoteViewerActivity";
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +53,104 @@ public class MainActivity extends AppCompatActivity {
 
         getSupportActionBar().setDisplayShowTitleEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
-        getSupportActionBar().setIcon(R.mipmap.icon);
+        getSupportActionBar().setIcon(R.drawable.ic_icon);
+
+
+
+
+        // Get the string arrays with the titles and qutoes
+        mTitleArray = new String[]{"a", "b", "c"};
+        mQuoteArray = new String[]{"apple", "bottom", "cleats"};
+
+        setContentView(R.layout.main);
+
+        // Get references to the TitleFragment and to the QuotesFragment
+        mTitleFrameLayout = (FrameLayout) findViewById(R.id.title_fragment_container);
+        mQuotesFrameLayout = (FrameLayout) findViewById(R.id.quote_fragment_container);
+
+
+        // Get a reference to the SupportFragmentManager instead of original FragmentManager
+        mFragmentManager = getSupportFragmentManager();
+
+        // Start a new FragmentTransaction with backward compatibility
+        //android.support.v4.app.FragmentTransaction fragmentTransaction = mFragmentManager
+        //		.beginTransaction();
+
+        FragmentTransaction fragmentTransaction =mFragmentManager.beginTransaction();
+
+        // Add the TitleFragment to the layout
+        // UB: 10/2/2016 Changed add() to replace() to avoid overlapping fragments
+        fragmentTransaction.replace(
+                R.id.title_fragment_container,
+                new TitlesFragment());
+
+        // Commit the FragmentTransaction
+        fragmentTransaction.commit();
+
+        // Add a OnBackStackChangedListener to reset the layout when the back stack changes
+        mFragmentManager.addOnBackStackChangedListener(
+                // UB 2/24/2019 -- Use support version of Listener
+                new FragmentManager.OnBackStackChangedListener() {
+                    public void onBackStackChanged() {
+                        setLayout();
+                    }
+                });
+    }
+
+
+    private void setLayout() {
+
+        // Determine whether the QuoteFragment has been added
+        if (!mQuoteFragment.isAdded()) {
+
+            // Make the TitleFragment occupy the entire layout
+            mTitleFrameLayout.setLayoutParams(new LinearLayout.LayoutParams(
+                    MATCH_PARENT, MATCH_PARENT));
+            mQuotesFrameLayout.setLayoutParams(new LinearLayout.LayoutParams(0,
+                    MATCH_PARENT));
+        } else {
+
+            // Make the TitleLayout take 1/3 of the layout's width
+            mTitleFrameLayout.setLayoutParams(new LinearLayout.LayoutParams(0,
+                    MATCH_PARENT, 1f));
+
+            // Make the QuoteLayout take 2/3's of the layout's width
+            mQuotesFrameLayout.setLayoutParams(new LinearLayout.LayoutParams(0,
+                    MATCH_PARENT, 2f));
+        }
+    }
+
+    @Override
+    public void onListSelection(int index) {
+
+        // If the QuoteFragment has not been added, add it now
+        if (!mQuoteFragment.isAdded()) {
+
+            // Start a new FragmentTransaction
+            // UB 2/24/2019 -- Now must use compatible version of FragmentTransaction
+            FragmentTransaction fragmentTransaction = mFragmentManager
+                    .beginTransaction();
+
+            // Add the QuoteFragment to the layout
+            fragmentTransaction.add(R.id.quote_fragment_container,
+                    mQuoteFragment);
+
+            // Add this FragmentTransaction to the backstack
+            fragmentTransaction.addToBackStack(null);
+
+            // Commit the FragmentTransaction
+            fragmentTransaction.commit();
+
+            // Force Android to execute the committed FragmentTransaction
+            mFragmentManager.executePendingTransactions();
+        }
+
+        if (mQuoteFragment.getShownIndex() != index) {
+
+            // Tell the QuoteFragment to show the quote string at position index
+            mQuoteFragment.showQuoteAtIndex(index);
+
+        }
     }
 
 
@@ -55,4 +177,5 @@ public class MainActivity extends AppCompatActivity {
                 return super.onOptionsItemSelected(item);
         }
     }
+
 }
---
+++ A3/app/src/main/java/com/example/a3/QuotesFragment.java
@@ -0,0 +1,115 @@
+package com.example.a3;
+
+import androidx.fragment.app.Fragment;
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+//Several Activity and Fragment lifecycle methods are instrumented to emit LogCat output
+//so you can follow the class' lifecycle
+// MH 7/7/2020 -- Changed deprecated Fragment class to support.v4 version.
+// Added as dependency in gradle module build file
+public class QuotesFragment extends Fragment {
+
+	private static final String TAG = "QuotesFragment";
+
+	private TextView mQuoteView = null;
+	private int mCurrIdx = -1;
+	private int mQuoteArrLen;
+
+	int getShownIndex() {
+		return mCurrIdx;
+	}
+
+	// Show the Quote string at position newIndex
+	void showQuoteAtIndex(int newIndex) {
+		if (newIndex < 0 || newIndex >= mQuoteArrLen)
+			return;
+		mCurrIdx = newIndex;
+		mQuoteView.setText(MainActivity.mQuoteArray[mCurrIdx]);
+	}
+	
+	@Override
+	public void onAttach(Context activity) {
+		Log.i(TAG, getClass().getSimpleName() + ":entered onAttach()");
+		super.onAttach(activity);
+	}
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		Log.i(TAG, getClass().getSimpleName() + ":entered onCreate()");
+		super.onCreate(savedInstanceState);
+	}
+
+	// Called to create the content view for this Fragment
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		Log.i(TAG, getClass().getSimpleName() + ":entered onCreateView()");
+
+		// Inflate the layout defined in quote_fragment.xml
+		// The last parameter is false because the returned view does not need to be attached to the container ViewGroup
+		return inflater.inflate(R.layout.quote_fragment,
+				container, false);
+	}
+	
+	// Set up some information about the mQuoteView TextView 
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		Log.i(TAG, getClass().getSimpleName() + ":entered onActivityCreated()");
+		super.onActivityCreated(savedInstanceState);
+
+		mQuoteView = (TextView) getActivity().findViewById(R.id.quoteView);
+		mQuoteArrLen = MainActivity.mQuoteArray.length;
+	}
+
+	@Override
+	public void onStart() {
+		Log.i(TAG, getClass().getSimpleName() + ":entered onStart()");
+		super.onStart();
+	}
+	
+	@Override
+	public void onResume() {
+		Log.i(TAG, getClass().getSimpleName() + ":entered onResume()");
+		super.onResume();
+	}
+
+	
+	@Override
+	public void onPause() {
+		Log.i(TAG, getClass().getSimpleName() + ":entered onPause()");
+		super.onPause();
+	}
+
+	@Override
+	public void onStop() {
+		Log.i(TAG, getClass().getSimpleName() + ":entered onStop()");
+		super.onStop();
+	}
+	
+	@Override
+	public void onDetach() {
+		Log.i(TAG, getClass().getSimpleName() + ":entered onDetach()");
+		super.onDetach();
+	}
+
+	
+	@Override
+	public void onDestroy() {
+		Log.i(TAG, getClass().getSimpleName() + ":entered onDestroy()");
+		super.onDestroy();
+	}
+
+	@Override
+	public void onDestroyView() {
+		Log.i(TAG, getClass().getSimpleName() + ":entered onDestroyView()");
+		super.onDestroyView();
+	}
+
+}
